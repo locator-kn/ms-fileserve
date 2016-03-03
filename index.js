222d1e6d9f2a7707877a26b779a1b8e3f343110c
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-03-03 10:15:56 +0100

    use env variable for setting up database

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var manifest = {
     plugins: [{
         'hapi-mongodb': [{
             options: {
-                'url': 'mongodb://localhost:27017/locator',
+                'url': 'mongodb://' + process.env['DB_HOST'] + ':' + process.env['DB_PORT'] + '/' + process.env['DB_NAME'],
                 'settings': {
                     'db': {
                         'native_parser': false
